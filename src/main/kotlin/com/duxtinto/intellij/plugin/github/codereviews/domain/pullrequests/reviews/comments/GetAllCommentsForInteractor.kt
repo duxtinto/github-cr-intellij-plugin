a3033be38a1890365e0cddbd341a78b18899f230
David Hernando Sancha <david.hernando@2manylanguages.com>
2018-02-02 23:35:27 +0100

    Add the 'get all comments by review' interactor

--- src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/domain/pullrequests/reviews/comments/GetAllCommentsForInteractor.kt
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/domain/pullrequests/reviews/comments/GetAllCommentsForInteractor.kt
@@ -1,14 +1,15 @@
 package com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.comments
 
-import com.duxtinto.intellij.plugin.github.codereviews.domain.Interactor
 import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.CodeReviewEntity
+import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.CodeReviewsDomainContract
 import javax.inject.Inject
 
 class GetAllCommentsForInteractor
     @Inject
-    constructor()
-    : Interactor<CodeReviewEntity, List<CodeReviewCommentEntity>> {
-    override fun run(request: CodeReviewEntity?): List<CodeReviewCommentEntity> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    constructor(private val commentsRepository: CodeReviewsDomainContract.Comments.Repository)
+    : CommentsByReviewInteractor {
+
+    override fun run(request: CodeReviewEntity): List<CodeReviewCommentEntity> {
+        return commentsRepository.getAllBy(request)
     }
 }
