50510620e079a6e00587e858640cf79ab51df6cd
David Hernando Sancha <david.hernando@2manylanguages.com>
2018-02-02 23:18:36 +0100

    Creating the review comments repository

--- src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/data/codereviews/CodeReviewsDataContract.kt
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/data/codereviews/CodeReviewsDataContract.kt
@@ -1,9 +1,11 @@
 package com.duxtinto.intellij.plugin.github.codereviews.data.codereviews
 
 import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.CodeReviewEntity
+import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.comments.CodeReviewCommentEntity
 
 interface CodeReviewsDataContract {
     interface Fetcher {
         fun fetchAllByPullRequestId(id: Long): List<CodeReviewEntity>
+        fun fetchAllReviewComments(review: CodeReviewEntity): List<CodeReviewCommentEntity>
     }
 }
