2f9eda3a9a6e68daffc14f09886fb5ce3e83ac20
David Hernando Sancha <david.hernando@2manylanguages.com>
2018-02-03 01:00:58 +0100

    adding comment functionality into CodeReviewsPresenter

--- src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/ui/toolwindow/codereviews/CodeReviewsPresenter.kt
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/ui/toolwindow/codereviews/CodeReviewsPresenter.kt
@@ -1,21 +1,28 @@
 package com.duxtinto.intellij.plugin.github.codereviews.ui.toolwindow.codereviews
 
-import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.PullRequestEntity
-import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.GetAllReviewsForInteractor
+import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.CodeReviewEntity
+import com.duxtinto.intellij.plugin.github.codereviews.domain.pullrequests.reviews.comments.CodeReviewCommentEntity
 import javax.inject.Inject
 
 class CodeReviewsPresenter
     @Inject
-    constructor(private val getAllCodeReviewsFor: GetAllReviewsForInteractor)
+    constructor()
     : CodeReviews.Presenter {
 
+    private var model: CodeReviews.Model = CodeReviewsModel()
     private var view : CodeReviews.View? = null
 
     override fun setView(view: CodeReviews.View) {
         this.view = view
     }
 
-    override fun displayCodeReviews(pullRequest: PullRequestEntity) {
-        view?.render(CodeReviewsModel(getAllCodeReviewsFor.run(pullRequest)))
+    override fun presentReviews(reviews: List<CodeReviewEntity>) {
+        model.setReviews(reviews)
+        view?.render(model)
+    }
+
+    override fun presentComments(review: CodeReviewEntity, comments: List<CodeReviewCommentEntity>) {
+        model.setReviewComments(review, comments)
+        view?.render(model)
     }
 }
