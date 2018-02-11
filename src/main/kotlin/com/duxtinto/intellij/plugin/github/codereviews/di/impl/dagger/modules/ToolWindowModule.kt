295ca6487037733419cbef1c0fa9502f5902faac
David Hernando Sancha <david.hernando@2manylanguages.com>
2018-02-11 11:00:01 +0100

    Adding DI changes

--- src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/di/impl/dagger/modules/ToolWindowModule.kt
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/di/impl/dagger/modules/ToolWindowModule.kt
@@ -3,8 +3,10 @@ package com.duxtinto.intellij.plugin.github.codereviews.di.impl.dagger.modules
 import com.duxtinto.intellij.plugin.github.codereviews.di.scopes.ProjectScoped
 import com.duxtinto.intellij.plugin.github.codereviews.ide.acl.presentation.toolwindow.ToolWindowContent
 import com.duxtinto.intellij.plugin.github.codereviews.ide.acl.presentation.toolwindow.ToolWindowContentPresenter
+import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewee.RevieweeContent
 import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewee.RevieweePresenter
 import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewee.RevieweeView
+import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewer.ReviewerContent
 import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewer.ReviewerPresenter
 import com.duxtinto.intellij.plugin.github.codereviews.presentation.reviewer.ReviewerView
 import dagger.Binds
@@ -18,17 +20,17 @@ abstract class ToolWindowModule {
 
     @Binds
     @ProjectScoped
-    abstract fun provideToolWindowContentRevieweeView(view: RevieweeView): ToolWindowContent.Reviewee.View
+    abstract fun provideToolWindowContentRevieweeView(view: RevieweeView): RevieweeContent.View
 
     @Binds
     @ProjectScoped
-    abstract fun provideToolWindowContentRevieweePresenter(presenter: RevieweePresenter): ToolWindowContent.Reviewee.Presenter
+    abstract fun provideToolWindowContentRevieweePresenter(presenter: RevieweePresenter): RevieweeContent.Presenter
 
     @Binds
     @ProjectScoped
-    abstract fun provideToolWindowContentReviewerView(view: ReviewerView): ToolWindowContent.Reviewer.View
+    abstract fun provideToolWindowContentReviewerView(view: ReviewerView): ReviewerContent.View
 
     @Binds
     @ProjectScoped
-    abstract fun provideToolWindowContentReviewerPresenter(presenter: ReviewerPresenter): ToolWindowContent.Reviewer.Presenter
+    abstract fun provideToolWindowContentReviewerPresenter(presenter: ReviewerPresenter): ReviewerContent.Presenter
 }
---
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/di/qualifiers/reviewee.kt
@@ -0,0 +1,8 @@
+package com.duxtinto.intellij.plugin.github.codereviews.di.qualifiers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Reviewee
