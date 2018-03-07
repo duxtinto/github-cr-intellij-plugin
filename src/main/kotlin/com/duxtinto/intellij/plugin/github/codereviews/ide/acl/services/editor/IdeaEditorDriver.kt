17c10872a4653618439b3c0065cd72d96abf52d0
David Schreiber-Ranner <david@pspdfkit.com>
2018-03-07 22:35:46 +0100

    Fix file resolution for non-root folder projects

--- src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/ide/acl/services/editor/IdeaEditorDriver.kt
+++ src/main/kotlin/com/duxtinto/intellij/plugin/github/codereviews/ide/acl/services/editor/IdeaEditorDriver.kt
@@ -3,6 +3,7 @@ package com.duxtinto.intellij.plugin.github.codereviews.ide.acl.services.editor
 import com.duxtinto.intellij.plugin.github.codereviews.domain.DomainContract
 import com.duxtinto.intellij.plugin.github.codereviews.ide.acl.entities.ProjectExt
 import com.duxtinto.intellij.plugin.github.codereviews.ide.acl.entities.editor.IdeaDocumentDriver
+import org.jetbrains.plugins.github.util.GithubUtil
 import javax.inject.Inject
 
 class IdeaEditorDriver
@@ -11,9 +12,14 @@ class IdeaEditorDriver
             private val project: ProjectExt)
     : DomainContract.EditorDriver {
     override fun openDocument(filePath: String): DomainContract.DocumentDriver {
+        val rootFolder = GithubUtil.getGitRepository(project, null)?.root
+            ?: throw error("Could not determine root folder of the project's git repository.")
+        val file = rootFolder.findFileByRelativePath(filePath)
+            ?: throw IllegalStateException("Could not find file at path $filePath (for git root: $rootFolder)")
+
         return IdeaDocumentDriver(
                 project,
-                project.baseDir.findFileByRelativePath(filePath)!!
+                file
         )
     }
 }
